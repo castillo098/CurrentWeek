7144d9be69955f7fcc191de537dd7771577debc5
castillo098 <losvideojuegos7268098@gmail.com>
2020-08-03 00:06:41 -0500

    Creacion de proyecto
    Se creo un proyecto de una aplicacion movil

---
+++ CurrentWeek/app/src/main/java/com/example/currentweek/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.currentweek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void Button2 (View view) {
+
+        Intent Button2 = new Intent(this, MainActivity.class);
+        startActivity(Button2);
+    }
+}
